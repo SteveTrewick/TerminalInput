c33d282a5e3b88c48f8b749e4adf2d85f9e7a1d6
Steve Trewick <steve.trewick@icloud.com>
2025-10-06 14:32:55 +0100

    I'm going to regret this but here I provide a basic framework

--- Sources/TerminalInput/TerminalInput.swift
+++ Sources/TerminalInput/TerminalInput.swift
@@ -1,3 +1,22 @@
 import Foundation
 
 
+public class TerminalInput {
+  
+  //TODO: we will need a stateful parser for xterm input over stdin
+  
+  public var dispatch : ( (Result<Token, Error>) -> Void )?
+  
+  public enum Error : Swift.Error {
+    //TODO: error definitions go here
+  }
+  
+  public enum Token {
+    //TODO: token defintions go here
+  }
+  
+  public func enqueue ( _ bytes: Data ) {
+    //TODO: parser invocations go here, when we have a token to emit, we call dispatch
+  }
+
+}
